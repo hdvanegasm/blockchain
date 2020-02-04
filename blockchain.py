4ecd8fafbfef4f64b9538809466c0b8d4e52a689
Hernan Dario Vanegas Madrigal <hdvanegasm@gmail.com>
2020-02-04 16:32:57 -0500

    Mine genesis block

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,14 @@
 import hashlib
 import random
+
 class Blockchain:
     def __init__(self, difficulty, blocks = []):
-        first_transaction = Transaction("a988a7d0a8dd8")
-        genesis_block = Block(first_transaction, 0, "")
-        blocks.append(genesis_block)
-
         self.blocks = blocks
         self.difficulty = difficulty
+
+        first_transaction = Transaction("a988a7d0a8dd8")
+        genesis_block = mine_block(first_transaction, self)
+        self.add_block(genesis_block)
 
     def add_block(self, block):
         if block.get_hash().startswith("0" * self.difficulty):
@@ -35,18 +36,21 @@ class Transaction:
 def mine_block(transaction, blockchain):
     nonce = 0
     while True:
-        hash_prev_block = blockchain.blocks[len(blockchain.blocks) - 1].get_hash()
-        new_block = Block(transaction, nonce, hash_prev_block)
-
-        if new_block.get_hash().startswith("0" * blockchain.difficulty):
+    	if len(blockchain.blocks) != 0:
+        	hash_prev_block = blockchain.blocks[len(blockchain.blocks) - 1].get_hash()
+        	new_block = Block(transaction, nonce, hash_prev_block)
+    	else:
+        	new_block = Block(transaction, nonce, "")
+    	
+    	if new_block.get_hash().startswith("0" * blockchain.difficulty):
             print("Nonce found:", nonce)
             return new_block
 
-        nonce += 1
+    	nonce += 1
 
 if __name__ == "__main__":
     transaccion = Transaction("skfsjdglkjfn")
-    blockchain = Blockchain(5)
+    blockchain = Blockchain(3)
 
     block = mine_block(transaccion, blockchain)
 
